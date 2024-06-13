ed2424f00f6208ce90016dfc5e64d8d0b39d0594
Asim <asimalam8@gmail.com>
2024-06-13 08:34:41 +0530

    resume update

--- data/Constant.js
+++ data/Constant.js
@@ -1,18 +1,18 @@
 export const Bio = {
-    name: "Asim Alam",
-    roles: [
-      "Full Stack Developer",
-      "Android Developer",
-      "UI/UX Designer",
-      "Programmer",
-    ],
-    description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/AsimAlam",
-    resume:
-      "https://drive.google.com/file/d/1Z-ibUlvX409NYnVJbX_OHf2Ye4rVaRMz/view",
-    linkedin: "https://www.linkedin.com/in/asim-alam-b820b7205/",
-    discord: "https://discordapp.com/users/831661720979898379",
-    twitter: "https://x.com/here_asim8",
-    google_cloud : "https://www.cloudskillsboost.google/public_profiles/c4f8be17-9d98-4d94-aeba-7c58c68a54d9"
-  };
+  name: "Asim Alam",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/AsimAlam",
+  resume:
+    "https://drive.google.com/file/d/1cQfIKhN_6B_fsyq_N0nSKRsftkQJ_4mE/view",
+  linkedin: "https://www.linkedin.com/in/asim-alam-b820b7205/",
+  discord: "https://discordapp.com/users/831661720979898379",
+  twitter: "https://x.com/here_asim8",
+  google_cloud: "https://www.cloudskillsboost.google/public_profiles/c4f8be17-9d98-4d94-aeba-7c58c68a54d9"
+};
